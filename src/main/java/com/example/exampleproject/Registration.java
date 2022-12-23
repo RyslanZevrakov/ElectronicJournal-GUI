d9ac9ae540b3bb7e482004ade58c9c1a39861dad
Руслан Жевраков <116262017+RyslanZevrakov@users.noreply.github.com>
2022-12-23 13:44:30 +0300

    Add files via upload

---
+++ src/main/java/com/example/exampleproject/Registration.java
@@ -0,0 +1,15 @@
+package com.example.exampleproject;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class Registration {
+    @FXML
+    private ImageView transition;
+
+
+    }
+
+
